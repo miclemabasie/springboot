f881d3cfd515bee44fc18c569b56c9476dff17ec
miclemabasie <miclemabasie3@gmail.com>
2025-05-26 01:13:44 +0100

    initial commit

---
+++ src/main/java/com/example/jobapp/job/Job.java
@@ -0,0 +1,68 @@
+package com.example.jobapp.job;
+
+public class Job {
+
+    private Long id;
+    private String title;
+    private String description;
+    private double minSalary;
+    private double maxSalary;
+    private String location;
+
+    public Job(Long id, String title, String description, double minSalary, double maxSalary, String location) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.minSalary = minSalary;
+        this.maxSalary = maxSalary;
+        this.location = location;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(double minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public double getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(double maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
---
+++ src/main/java/com/example/jobapp/job/JobController.java
@@ -0,0 +1,18 @@
+package com.example.jobapp.job;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class JobController {
+    final List<Job> jobs = new ArrayList<>();
+    @GetMapping("/jobs")
+    public List<Job> findAll() {
+        return jobs;
+    }
+
+    @PostMapping("/jobs")
+}
